bdfe2fbea805e8e2217992d78895df0343e4512d
kesy <92neco92@gmail.com>
2023-02-23 18:32:06 +0100

    Assign data to page elements, and set style rules for them

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let cityName = document.querySelector("#search-box");
-cityName.value = "bajina basta";
+let searchName = document.querySelector("#search-box");
+searchName.value = "London";
 
 let tempUnit = [...document.querySelectorAll("#unit-mesure")];
 let submitBtn = document.querySelector('button[type="submit"]');
 
 tempUnit.forEach((unit) => {
   unit.addEventListener("change", () => {
-    console.log(unit.value);
     return (selectedUnit = unit.value);
   });
 });
@@ -14,46 +13,42 @@ tempUnit.forEach((unit) => {
 let selectedUnit = "metric";
 
 let weatherData;
-let cityNameData;
-let temp;
-let feelsLikeTemp;
-let pressure;
-let weatherDescription;
-let windSpeed;
+let cityNameData = document.querySelector(".location-name");
+let temp = document.querySelector(".temp");
+let feelsLikeTemp = document.querySelector(".feelsLike");
+let pressure = document.querySelector(".presure");
+let weatherDescription = document.querySelector(".description");
+let windSpeed = document.querySelector(".wind");
 
 async function getWeather() {
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=3250ef26a3e988b227b3eff768815792&units=${selectedUnit}`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${searchName.value}&appid=3250ef26a3e988b227b3eff768815792&units=${selectedUnit}`,
     { mode: "cors" }
   );
   weatherData = await response.json();
 }
 
 function organizeData() {
-  cityNameData = weatherData.name;
-  temp = weatherData.main.temp;
-  feelsLikeTemp = weatherData.main.feels_like;
-  pressure = weatherData.main.pressure;
-  weatherDescription = weatherData.weather[0].description;
-  windSpeed = weatherData.wind.speed;
-  console.log({
-    cityNameData,
-    temp,
-    feelsLikeTemp,
-    pressure,
-    weatherDescription,
-    windSpeed,
-  });
+  cityNameData.textContent = weatherData.name;
+  temp.textContent = `${weatherData.main.temp} ${mesure}`;
+  feelsLikeTemp.textContent = `${weatherData.main.feels_like} ${mesure}`;
+  pressure.textContent = weatherData.main.pressure + " mb";
+  weatherDescription.textContent =
+    weatherData.weather[0].description.toUpperCase();
+  windSpeed.textContent = `${weatherData.wind.speed} ${speedUnit}`;
 }
 
-getWeather().then(() => {
-  console.log(weatherData);
+async function newInfo() {
+  await getWeather();
+  if (selectedUnit == "metric") {
+    mesure = "°C";
+    speedUnit = "m/s";
+  } else {
+    mesure = "°F";
+    speedUnit = "mi/h";
+  }
   organizeData();
-});
+}
+newInfo();
 
-submitBtn.addEventListener("click", () => {
-  getWeather().then(() => {
-    console.log(weatherData);
-    organizeData();
-  });
-});
+submitBtn.addEventListener("click", newInfo);
